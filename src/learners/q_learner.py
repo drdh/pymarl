1089dbe8147c216c7fc312b4b2e44d41f77ff6ff
drdh <drdh@mail.ustc.edu.cn>
2019-11-22 14:31:13 +0800

    comments

--- src/learners/q_learner.py
+++ src/learners/q_learner.py
@@ -1,7 +1,7 @@
 import copy
-from components.episode_buffer import EpisodeBatch
-from modules.mixers.vdn import VDNMixer
-from modules.mixers.qmix import QMixer
+from ..components.episode_buffer import EpisodeBatch
+from ..modules.mixers.vdn import VDNMixer
+from ..modules.mixers.qmix import QMixer
 import torch as th
 from torch.optim import RMSprop
 
