1200c6e297db21f1405401b59bca77dfdaef73e0
drdh <drdh@mail.ustc.edu.cn>
2019-11-25 19:04:34 +0800

    latent mixture input; not finished

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -3,7 +3,9 @@ REGISTRY = {}
 from .rnn_agent import RNNAgent
 from .latent_rnn_agent import LatentRNNAgent
 from .latent_input_rnn_agent import LatentInputRNNAgent
+from .latent_mixture_input_rnn_agent import LatentMixtureInputRNNAgent
 
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["latent_rnn"] = LatentRNNAgent
 REGISTRY["latent_input_rnn"] = LatentInputRNNAgent
+REGISTRY["latent_mixture_input_rnn"] = LatentMixtureInputRNNAgent
