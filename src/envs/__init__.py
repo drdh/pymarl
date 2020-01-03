327cb7e2c0d91bdd5e728686927d5ace676fcc24
drdh <drdh@mail.ustc.edu.cn>
2020-01-03 23:08:39 +0800

    gather_and_defend2

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,7 +1,7 @@
 from functools import partial
 #from smac.env import MultiAgentEnv, StarCraft2Env, GatherDefendEnv
 from smac.env import MultiAgentEnv, StarCraft2Env
-from .gather_and_defend2 import GatherDefendEnv
+from .gather_and_defend import GatherDefendEnv
 import sys
 import os
 
